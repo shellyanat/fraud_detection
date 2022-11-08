090569aa3e39a764380c5a357668b72e8adbef6d
Shellya Nur <95962010+shellyanat@users.noreply.github.com>
2022-11-08 08:50:28 +0700

    Update fd_placement_cakap.py

--- fd_placement_cakap.py
+++ fd_placement_cakap.py
@@ -400,6 +400,15 @@ with tab3:
 
 with tab2:
 #percentage of fp
+    labels = ['buy_premium_y', 'buy_premium_n']
+    values = [df_teacher['buy_premium_y'].sum(), df_teacher['buy_premium_n'].sum()]
+    fig2 = go.Figure(data=[go.Pie(labels=labels, values=values, hole=0.3)])
+    fig2.update_layout(legend = dict(font = dict(size = 20)), width=600, height=600)
+    fig_pie1.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=20,
+                      marker=dict(colors=px.colors.qualitative.Set2, line=dict(color='#000000', width=2)))
+    fig_pie1.update_layout(title_text='Fraud Possibility',title_x=0.5)
+    st.plotly_chart(fig2)
+
     df_fraud_possibility = df_fp['fraud_possibility'].value_counts().rename_axis('fraud_possibility').reset_index(name='counts')
     fig_pie1 = go.Figure(data=[go.Pie(labels=df_fraud_possibility['fraud_possibility'],
                              values=df_fraud_possibility['counts'])])
@@ -407,14 +416,7 @@ with tab2:
                       marker=dict(colors=px.colors.qualitative.Set2, line=dict(color='#000000', width=2)))
     fig_pie1.update_layout(title_text='Fraud Possibility',title_x=0.5)
     st.plotly_chart(fig_pie1)
-
-    labels = ['buy_premium_y', 'buy_premium_n']
-    values = [df_teacher['buy_premium_y'].sum(), df_teacher['buy_premium_n'].sum()]
-    fig2 = go.Figure(data=[go.Pie(labels=labels, values=values, hole=0.3)])
-    fig2.update_layout(legend = dict(font = dict(size = 20)), width=600, height=600)
-
-    st.plotly_chart(fig2)
-
+    
 def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
